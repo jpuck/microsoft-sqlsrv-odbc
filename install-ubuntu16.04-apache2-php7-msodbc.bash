4d30d4daead9eb6d2c5e25783ca52347615b5a8d
Jeff Puckett <jeff@jeffpuckett.com>
2016-12-16 17:37:03 +0000

    doc script steps better

--- install-ubuntu16.04-apache2-php7-msodbc.bash
+++ install-ubuntu16.04-apache2-php7-msodbc.bash
@@ -7,17 +7,17 @@
 # I suggest executing each step line-by-line and observe what's happening.
 # this is proprietary software afterall, polluting your open-source environment.
 
-# move to working directory, only needed for script
-# ignore these lines if manually executing
+# ignore these lines if manually executing, only needed for script
+# kill script on error
 set -e
+# move to script's working directory
 cd $( dirname "${BASH_SOURCE[0]}" )
 
-# dependencies
+# install dependencies
 apt install make libc6 libkrb5-3 libgss3 e2fsprogs openssl libstdc++6 apache2 php libapache2-mod-php php7.0-zip php7.0-mbstring php-xml php-curl php-gd php-ssh2 unixodbc php-odbc -y
 
 # enable odbc
 phpenmod odbc
-service apache2 restart
 
 # original download driver link page https://www.microsoft.com/en-us/download/details.aspx?id=50419
 tar xvzf msodbcsql-13.0.0.0.tar.gz
@@ -25,15 +25,18 @@ cd msodbcsql-13.0.0.0
 ./build_dm.sh --accept-warning
 
 # driver manager will have custom directory
-cd /tmp/unixODBC.*/unixODBC-*; make install
+cd /tmp/unixODBC.*/unixODBC-*
 
-# return to download dir
+# build
+make install
+
+# return to download directory
 cd -
 
 # backup odbcinst.ini
 odbcinibak=$(odbc_config --odbcinstini) && cp $odbcinibak $odbcinibak.bak
 
-# actually install driver
+# install driver
 ./install.sh verify
 ./install.sh install --accept-license
 
